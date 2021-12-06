a0ac1264bf0776a977539ea27779cfc779088903
oleg_gerasimovich <oleg-younost@mail.ru>
2021-12-06 22:09:58 +0300

    Refactor script file

--- airflow_home/scripts/extract.py
+++ airflow_home/scripts/extract.py
@@ -1,4 +1,5 @@
 import os
+
 import requests
 from dotenv import load_dotenv
 
@@ -6,8 +7,8 @@ from dotenv import load_dotenv
 API_URL = "https://yfapi.net/v6/finance/quote"
 
 
-def extract(symbol: str, **kwargs):
-    ti = kwargs['ti']
+def extract(symbol: str, **kwargs) -> None:
+    ti = kwargs['ti'] # need for xcom push and pull commands
 
     load_dotenv()
 
@@ -22,6 +23,5 @@ def extract(symbol: str, **kwargs):
     response = requests.request("GET", API_URL, headers=headers, params=querystring)
 
     ticker_data[symbol] = response.json()
-    print(ticker_data)
 
     ti.xcom_push(key='ticker_data', value=ticker_data)
